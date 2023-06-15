78d1cac39151edc7f399b3d893a4c46392468454
gog <clod127@msn.com>
2023-06-16 00:11:14 +0500

    fix

--- app/Models/Athlete.php
+++ app/Models/Athlete.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class Competitor extends Model implements AuthenticatableContract, AuthorizableContract
+class Athlete extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable;
     use Authorizable;
